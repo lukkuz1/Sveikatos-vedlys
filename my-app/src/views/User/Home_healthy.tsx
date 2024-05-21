67cdde937c0210afc21d713ed2fd5d0b75d59bfb
Bazilijus12 <114340327+Bazilijus12@users.noreply.github.com>
2024-05-21 20:58:57 +0300

    Rakeeeetaaaa

--- my-app/src/views/User/Home_healthy.tsx
+++ my-app/src/views/User/Home_healthy.tsx
@@ -10,11 +10,15 @@ const Home_healthy: React.FC = () => {
   const OpenChatBot = () => {
     navigate("/healthy/chatbot")
   }
+  const OpenRegistredConsultationPage = () => {
+    navigate("/healthy/registeredConsultations")
+  }
 
   return (
     <div style={{ display: "flex", justifyContent: "center", alignContent: "center", marginTop: 30 }}>
       <button onClick={OpenHealthMissionsPage} >Sveikatingumo iššūkiai</button>
       <button onClick={OpenChatBot} >Sveikatingumo robotas</button>
+      <button onClick={OpenRegistredConsultationPage} >Konsultacijos į kurias užsiregistruota</button>
     </div>
   )
 }
--- my-app/src/views/User/Registered_consultations_view_page.tsx
+++ my-app/src/views/User/Registered_consultations_view_page.tsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Consultation_controller from "../../controllers/Consultant/Consultation_controller";
+import { Consultation } from '../../models/Consultation';
 
-export const Registered_consultations_view_page: React.FC = () => {
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center", alignContent: "center", marginTop: 30 }}>
-        <h1>Registered_consultations_view_page</h1>
-    </div>
-  )
-}
+export default function Registered_consultations_view_page() {
 
-export default Registered_consultations_view_page;
+    const [consultations, setConsultations] = useState<Consultation[]>([]);
+    const [filter] = useState<string>(('registruota'));
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const consultationsData = await Consultation_controller().GetConsultations();
+                setConsultations(consultationsData);
+            }
+            catch (error) {
+                console.log(error)
+            }
+        };
+
+        fetch();
+    }, []);
+
+    const filteredMissions = consultations.filter(consultation => consultation.status === filter);
+
+    const OpenConsultationPage = (consultationID: string) => {
+        return <Link to={`/healthy/registeredConsultation/${consultationID}`}>Peržiūrėti</Link>;
+    };
+
+    return (
+        <div className="all">
+            <div className="read">
+                <h1>Registered Consultation List</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Consultation ID</th>
+                            <th>Description</th>
+                            <th>Type</th>
+                            <th>Link</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredMissions.map((consultation) => (
+                            <tr key={consultation.id}>
+                                <td>{consultation.id}</td>
+                                <td>{consultation.consultationDescription}</td>
+                                <td>{consultation.consultationType}</td>
+                                <td>{consultation.consultationLink}</td>
+                                <td>{consultation.consultationTime}</td>
+                                <td>{consultation.status}</td>
+                                <td>{OpenConsultationPage(consultation.id)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
